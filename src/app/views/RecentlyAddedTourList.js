41d62681a0bf163b2024ff88e2cdcec708e82b2e
HONGGEUN KIM <icandoit0914@gmail.com>
2023-01-02 20:06:44 -0800

    bug fixed: recently added tour display

--- src/app/views/RecentlyAddedTourList.js
+++ src/app/views/RecentlyAddedTourList.js
@@ -32,10 +32,9 @@ class TourList extends Component{
 
     tourlist = (props) => {
         const tourlist = [];
-        for(var idx=0; idx < this.state.data.length; idx++){
-            if(this.state.data.length < 10){
-                tourlist.push(<this.tourComponent key={idx} data={this.state.data[idx]} />);
-            }
+        // console.log(this.state.data)
+        for(var idx=0; idx < 10; idx++){
+            tourlist.push(<this.tourComponent key={idx} data={this.state.data[idx]} />);
         }
 
         return(
